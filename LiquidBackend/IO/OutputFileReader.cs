969663f9e2a1fa0cd2a90c48fa6603ff9f5fa103
Cameron Casey <cameron.casey@pnnl.gov>
2015-06-17 00:09:43 +0000

    Committing OutputFileReader.cs which is needed for previous commit.

---
+++ LiquidBackend/IO/OutputFileReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidBackend.IO
+{
+    public class OutputFileReader<T> : FileReader<T> where T : Tuple<string, int>
+    {
+        private const string COMMON_NAME = "COMMON NAME";
+        private const string MSMS_SCAN = "MS/MS SCAN";
+       
+        /// <summary>
+        /// Creates a mapping of column titles to their indices.
+        /// </summary>
+        /// <param name="columnString">The line containing the headers.</param>
+        /// <returns>A Dictionary mapping column titles to their indices.</returns>
+        protected override Dictionary<string, int> CreateColumnMapping(String columnString)
+        {
+            var columnMap = new Dictionary<string, int>();
+            string[] columnTitles = columnString.Split('\t', '\n');
+
+            for (int i = 0; i < columnTitles.Count(); i++)
+            {
+                String columnTitle = columnTitles[i].ToUpper();
+
+                switch (columnTitle)
+                {
+                    case COMMON_NAME:
+                        columnMap.Add(COMMON_NAME, i);
+                        break;
+                    case MSMS_SCAN:
+                        columnMap.Add(MSMS_SCAN, i);
+                        break;
+                }
+            }
+            return columnMap;
+        }
+
+        /// <summary>
+        /// Parses a line to create a Name, Scan map.
+        /// </summary>
+        /// <param name="line">A line containing data representing a Lipid Identification.</param>
+        /// <param name="columnMapping">The mapping of column titles to their indices.</param>
+        /// <returns>A dictionary pairing common name with the scan the lipid was detected in.</returns>
+        protected override T ParseLine(String line, IDictionary<string, int> columnMapping)
+        {
+            string[] columns = line.Split('\t', '\n');
+
+            if (!columnMapping.ContainsKey(COMMON_NAME)) throw new SystemException("Common Name is required for lipid import.");
+            if (!columnMapping.ContainsKey(MSMS_SCAN)) throw new SystemException("MS/MS Scan is required for lipid import.");
+            string name = columns[columnMapping[COMMON_NAME]];
+            int scan = Int32.Parse(columns[columnMapping[MSMS_SCAN]]);
+            //var Id = new T();
+            var ID = new Tuple<string, int>(name, scan);
+            //Id.Add(name,scan);
+            return (T)ID;
+        }
+
+    }
+}
