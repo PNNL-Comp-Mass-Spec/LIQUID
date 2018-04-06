ea0b065674c509bc5579e595d7de35b9bc3c7288
GrantFujimoto <grant.fujimoto@pnnl.gov>
2018-04-06 11:22:25 -0700

    Added the base lipid composition rule and the starting changes to structure

---
+++ LiquidBackend/Domain/LipidCompositionRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidBackend.Domain
+{
+	public class LipidCompositionRule
+	{
+
+		public string CommonName { get; set; }
+		public string Category { get; set; }
+		public string MainClass { get; set; }
+		public string SubClass { get; set; }
+		public string C { get; set; }
+		public string H { get; set; }
+		public string N { get; set; }
+		public string O { get; set; }
+		public string S { get; set; }
+		public string P { get; set; }
+		public string Example { get; set; }
+		public string Formula { get; set; }
+		public string IonizationMode { get; set; }
+		public int NumChains { get; set; }
+		public int ContainsEther { get; set; }
+		public int ContainsDiether { get; set; }
+		public int ContainsPlasmalogen { get; set; }
+		public int ContainsLCB { get; set; }
+		public int ContainsLCBOH { get; set; }
+		public int ContainsOH { get; set; }
+		public int ContainsDeoxy { get; set; }
+		public bool IsOxoCHO { get; set; }
+		public bool IsOxoCOOH { get; set; }
+
+	}
+}
---
+++ LiquidBackend/IO/LipidCompositionReader.cs
@@ -0,0 +1,22 @@
+﻿using LiquidBackend.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidBackend.IO
+{
+	public class LipidCompositionReader<T> : FileReader<T> where T : LipidCompositionRule, new()
+	{
+		protected override Dictionary<string, int> CreateColumnMapping(string columnString)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override T ParseLine(string line, IDictionary<string, int> columnMapping)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
