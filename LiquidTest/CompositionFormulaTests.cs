f0a065ea72fc7f2967c350b78120bb7c463abb2c
Joon-Yong Lee <joonyong.lee@pnnl.gov>
2018-04-18 12:10:14 -0700

    add tests of Evaluate function

--- LiquidTest/CompositionFormulaTests.cs
+++ LiquidTest/CompositionFormulaTests.cs
@@ -13,14 +13,21 @@ namespace LiquidTest
 		[Test]
 		public void TestCompositionFormulas()
 		{
-			string formStr1 = "2X-2-2Y";
+            Random rnd = new Random();
+            int numCarbons = rnd.Next(1, 10);
+            int numDoubleBonds = rnd.Next(1, 10);
+
+            string formStr1 = "2X-2-2Y";
 			CompositionFormula form1 = new CompositionFormula(formStr1);
+            Assert.AreEqual(form1.Evaluate(numCarbons, numDoubleBonds), 2 * numCarbons - 2 - 2 * numDoubleBonds);
 
 			string formStr2 = "X+15";
 			CompositionFormula form2 = new CompositionFormula(formStr2);
+            Assert.AreEqual(form2.Evaluate(numCarbons, numDoubleBonds), numCarbons + 15);
 
-			string formStr3 = "-X+40+2Y";
+            string formStr3 = "-X+40+2Y";
 			CompositionFormula form3 = new CompositionFormula(formStr3);
-		}
+            Assert.AreEqual(form3.Evaluate(numCarbons, numDoubleBonds), - numCarbons + 40 + 2 * numDoubleBonds);
+        }
 	}
 }
