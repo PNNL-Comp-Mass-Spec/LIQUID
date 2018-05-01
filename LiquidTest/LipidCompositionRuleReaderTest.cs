3550ec7a3fb4731e2a0ac3cada3ad105e9af5b14
GrantFujimoto <grant.fujimoto@pnnl.gov>
2018-04-30 22:18:44 -0700

    Added unit test for all lipid composition rules

--- LiquidTest/LipidCompositionRuleReaderTest.cs
+++ LiquidTest/LipidCompositionRuleReaderTest.cs
@@ -4,6 +4,7 @@ using LiquidBackend.IO;
 using LiquidBackend.Util;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -54,6 +55,60 @@ namespace LiquidTest
 					Console.WriteLine(newComp.ToPlainString());
 				}
 			}
+		}
+
+		[Test]
+		public void TestGetCompositionRuleForAllLipids()
+		{
+			Console.WriteLine("COMPOSITION RULES FOR ALL LIPIDS");
+			var lipidFilePath = @"C:\Users\fuji510\Desktop\LIQUID_REVISED\LIQUID_Subclass_chain_parsing_cleaned.txt";
+			var lipidFileInfo = new FileInfo(lipidFilePath);
+			var lipidCompositionRuleReader = new LipidCompositionRuleReader<LipidCompositionRule>();
+			var lipidCompositionRules = lipidCompositionRuleReader.ReadFile(lipidFileInfo);
+
+			List<int> percentage = new List<int>();
+
+			foreach (var rule in lipidCompositionRules)
+			{
+				try
+				{
+					Console.WriteLine("----------------------------------------------------------------------------------------------------");
+					string oldCommonName = rule.Example;
+
+					Composition oldComp = LipidUtil.ParseLipidCommonNameIntoCompositionWithoutAdduct(oldCommonName);
+					var fattyAcylChains = LipidUtil.ParseLipidCommonNameIntoAcylChains(oldCommonName).ToList();
+					var carbons = fattyAcylChains.Sum(x => x.NumCarbons);
+					var dBonds = fattyAcylChains.Sum(x => x.NumDoubleBonds);
+					Console.WriteLine("OLD COMMON NAME: " + oldCommonName + " COMPOSITION: " + oldComp.ToPlainString());
+
+					Composition newComp = rule.GetComposition(carbons, dBonds);
+
+					if (newComp.Equals(oldComp))
+					{
+						Console.WriteLine(rule.LipidClass + "\t" + rule.LipidSubClass + "\t" + rule.Example + "\t" + newComp.ToPlainString());
+						percentage.Add(1);
+					}
+					else
+					{
+						Console.WriteLine("------->NO MATCH<-------");
+						percentage.Add(0);
+					}
+				}
+				catch(Exception e)
+				{
+					percentage.Add(0);
+					Console.WriteLine(rule.Example + " ------------------>" + e.Message);
+					continue;
+				}
+			}
+
+			int sum = 0;
+			foreach (var entry in percentage)
+			{
+				sum += entry;
+			}
+			double result = (double)sum / (double)percentage.Count * 100;
+			Console.WriteLine("Percent Correct: " + result + "%");
 		}
 	}
 }
