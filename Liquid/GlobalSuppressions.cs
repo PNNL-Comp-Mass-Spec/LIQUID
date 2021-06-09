c04a9c8b12ce72f4c2afab99101210208cde10ec
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-09 13:37:06 -0700

    Suppress warnings

---
+++ Liquid/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:Liquid.ViewModel.MsMsInfoViewModel.CreateMsMsPlot(System.Collections.Generic.IReadOnlyCollection{LiquidBackend.Domain.MsMsSearchResult},InformedProteomics.Backend.Data.Spectrometry.ProductSpectrum)~OxyPlot.PlotModel")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:Liquid.ViewModel.MsOneInfoViewModel.CreateIsotopicProfilePlot")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:Liquid.ViewModel.MsOneInfoViewModel.CreateXicPlot")]
